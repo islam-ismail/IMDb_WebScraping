ff9a2781f4b41286e589bd76cb7fa13e57a54b2b
islam <islam.ismail038@gmail.com>
2020-07-18 07:22:15 +0200

    some comments

--- scrapingFile.py
+++ scrapingFile.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
+# custom colors for output
 blue = '\033[0;34m'
 reset = '\033[0;37m'
 
-
+# website Url
 url = 'https://www.imdb.com/list/ls016522954/?ref_=nv_tvv_dvd'
  
 headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'} 
@@ -18,6 +19,3 @@ finalResult = BeautifulSoup(result.content,'lxml')
 for x in finalResult.findAll('h3',{'class':'lister-item-header'}):
     for title in x.findAll('a'):
         print('Movie Found: ' + blue + title.text + reset)
-    
-
-#print(finalResult.text)
